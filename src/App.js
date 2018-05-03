1f9766cdca3f51cae32bb117f567a0b3482e1809
Theran Brigowatz <theran.brigowatz@gmail.com>
2018-05-04 00:55:41 +0900

    added album route and component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Library from './components/Library';
 import Landing from './components/Landing';
+import Album from './components/Album'
 
 class App extends Component {
   render() {
@@ -17,7 +18,8 @@ class App extends Component {
         </header>
         <main>
           <Route exact path='/' component={Landing} />
-          <Route path='/Library' component={Library} />
+          <Route path='/library' component={Library} />
+          <Route path='/album' component={Album} />
         </main>
       </div>
     );
---
+++ src/components/Album.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Album = () => (
+  <section className='album'>
+    Album info goes here.
+  </section>
+)
+
+export default Album;
